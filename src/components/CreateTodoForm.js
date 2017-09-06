b66562259d488999eb202c7a5ed543a54b7e9395
David Randall <oakesrandall@gmail.com>
2017-09-06 14:48:32 -0600

    got post route working

---
+++ src/components/CreateTodoForm.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class CreateTodoForm extends Component {
+    constructor() {
+        super();
+
+        this.state= {
+            todo: ''
+        };
+    }
+
+    onFormSubmit(event) {
+        console.log('form submitted');
+        event.preventDefault();
+        let newTodo = this.state.todo;
+        this.props.createTodo(newTodo);
+        this.setState({
+            todo: ''
+        });
+    }
+
+    onInputChange(event) {
+            this.setState( {
+                todo: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className='createForm todoForm'>
+                <h2>Create todo here!</h2>
+                <form onSubmit={event => this.onFormSubmit(event)}>
+                    <input
+                        onChange={event => this.onInputChange(event)}
+                        placeholder='Write a todo here ...'
+                        type='text'
+                        value={this.state.todo} />
+                    <button type='submit'>Create Todo!</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default CreateTodoForm
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import TodoModel from '../models/Todo';
 import TodoList from '../components/TodoList';
+import CreateTodoForm from '../components/CreateTodoForm';
 
 class TodosContainer extends Component {
     constructor() {
@@ -20,10 +21,24 @@ class TodosContainer extends Component {
         });
     }
 
+    createTodo(newBody) {
+        let newTodo = {
+            body: newBody,
+            completed: false,
+        };
+        TodoModel.create(newTodo).then((res) => {
+            console.log('created todo', res);
+            let todos = this.state.todos;
+            let newTodos = todos.push(res);
+            this.setState({newTodos});
+        });
+    }
+
     render() {
         return (
             <div className='todosContainer'>
-                <h2>This is the todos container</h2>
+                <CreateTodoForm
+                createTodo={this.createTodo.bind(this)} />
                 <TodoList
                 todos={this.state.todos} />
             </div>
